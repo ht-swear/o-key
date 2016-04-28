70c7fbdc656741f8d7608278953212cc9c7aab56
mario031 <reimariodayo@gmail.com>
2016-04-28 14:35:58 +0900

    update

--- O-Key/ViewController.swift
+++ O-Key/ViewController.swift
@@ -14,13 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var lockButton: UIButton!
     @IBOutlet weak var keyStateLabel: UILabel!
     
+    var reqData:NSString!
+    var url:NSURL!
+    
     var name:NSUserDefaults = NSUserDefaults()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(animated: Bool) {
         nowUpdate()
         var timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "nowUpdate", userInfo: nil, repeats: true)
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,23 +52,29 @@ class ViewController: UIViewController {
         openButton.enabled = false
         lockButton.enabled = false
         
-        var reqData:NSString = "message=\(message)&name=\(name.objectForKey("name") as! String)"
-        var url = NSURL(string: "http://133.27.171.30/~eigen/open.php")
+//        var reqData:NSString = "message=\(message)&name=\(name.objectForKey("name") as! String)"
+//        var url = NSURL(string: "http://133.27.171.30/~eigen/open.php")
         
         if (message == "update") {
             reqData = "message=\(message)"
             url = NSURL(string: "http://133.27.171.30/~eigen/update.php")
             
+        }else{
+            reqData = "message=\(message)&name=\(name.objectForKey("name") as! String)"
+            url = NSURL(string: "http://133.27.171.30/~eigen/open.php")
         }
         
         let myData:NSData = reqData.dataUsingEncoding(NSUTF8StringEncoding)!
         let request = NSMutableURLRequest(URL: url!)
         request.HTTPMethod = "POST"
         request.HTTPBody = myData
-        let result = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: nil)
-        let data = NSString(data: result!, encoding: NSUTF8StringEncoding) as! String
-        print(data)
-        keyStateLabel.text = data
+        
+        if(message == "update"){
+            let result = try? NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: self.postUpdate)
+        }
+        else{
+            let result = try? NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: self.postKeyState)
+        }
         
         openButton.enabled = true
         lockButton.enabled = true
@@ -71,6 +83,21 @@ class ViewController: UIViewController {
     
     func nowUpdate(){
         sendMessage("update")
+    }
+    //updateするとに呼ばれる
+    func postUpdate(res:NSURLResponse?,data:NSData?,error:NSError?){
+        if data != nil{
+            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
+            keyStateLabel.text = dataString
+            print(dataString)
+        }
+    }
+    //ボタンを押されたときに呼ばれる
+    func postKeyState(res:NSURLResponse?,data:NSData?,error:NSError?){
+        if data != nil{
+            let dataString = NSString(data: data!, encoding: NSUTF8StringEncoding) as! String
+            print(dataString)
+        }
     }
 
 }
